1cd4b0c8ebdfa8c4c85e95858fd82b3fd9ab587b
Sergey Sukharev <Serejaninja@gmail.com>
2015-09-18 11:57:43 +0400

    2.5.1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "sdt.h"
 int main()
 {
-  int n,m,sum;
-cout<< "VVedite dva chisla\n";
-cin>>n;
-cin>>m;
-sum=n+m;
-cout<<"summa"<<" "<<sum;
+    int n,m,sum,razn;
+    cout<< "VVedite dva chisla\n";
+    cin>>n;
+    cin>>m;
+    sum=n+m;
+    razn=n-m;
+    cout<<"summa"<<" "<<sum<<"\n ";
+    cout<<"raznost"<<" "<<razn;
 }
